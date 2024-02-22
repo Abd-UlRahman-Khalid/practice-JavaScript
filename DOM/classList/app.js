08e4669f794cd9b61cc514e1d34db4157e20a03f
Abd-UlRahman <a.khalid5322@gmail.com>
2024-02-22 12:45:43 +0200

    classLists

---
+++ DOM/classList/app.js
@@ -0,0 +1,15 @@
+// classlist
+// add()
+// remove()
+// toggle()
+
+// BROWSER CONSOLE
+const h1 = document.querySelector("h1");
+console.log(h1.classList); //show all classes assigned to h1
+//Add class to spicific element
+h1.classList.toggle("NewClass"); //added NewClass because there is no NewClass in HTML
+h1.classList.toggle("styles"); //removed styles class because it already exist
+// console.log((h1.style.color = "skyblue"));
+// console.log((h1.style.fontWeight = "normal"));
+
+// In EDITOR
